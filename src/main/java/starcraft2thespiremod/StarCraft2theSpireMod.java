71d07bb601fce4aa16246f641e9b507f9b310043
Jacktomyw <1259574307@qq.com>
2019-01-13 05:46:55 +0800

    build project constructions

---
+++ src/main/java/starcraft2thespiremod/StarCraft2theSpireMod.java
@@ -0,0 +1,42 @@
+package starcraft2thespiremod;
+
+import basemod.interfaces.EditCardsSubscriber;
+import basemod.interfaces.EditCharactersSubscriber;
+import basemod.interfaces.EditKeywordsSubscriber;
+import basemod.interfaces.EditRelicsSubscriber;
+import basemod.interfaces.EditStringsSubscriber;
+
+public class StarCraft2theSpireMod implements EditCharactersSubscriber, EditCardsSubscriber, EditStringsSubscriber,
+		EditRelicsSubscriber, EditKeywordsSubscriber {
+
+	@Override
+	public void receiveEditKeywords() {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void receiveEditRelics() {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void receiveEditStrings() {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void receiveEditCards() {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void receiveEditCharacters() {
+		// TODO Auto-generated method stub
+
+	}
+
+}
